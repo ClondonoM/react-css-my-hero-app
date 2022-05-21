9ec0b5f9292e60b533462604263512408c839317
Carlos Londono <c.londono.m@gmail.com>
2022-05-21 15:47:36 -0500

    first router created

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,6 +1,11 @@
 import { Link, NavLink } from 'react-router-dom';
 
 export const Navbar = () => {
+  const handleLogout = () => {
+    //TODO por hacer
+
+    console.log('Logout');
+  };
   return (
     <nav className='navbar navbar-expand-sm navbar-dark bg-dark'>
       <Link className='navbar-brand' to='/'>
@@ -19,11 +24,12 @@ export const Navbar = () => {
         </div>
       </div>
 
-      <div className='navbar-collapse collapse w-100 order-3 dual-collapse2'>
+      <div className='navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end'>
         <ul className='navbar-nav ml-auto'>
-          <NavLink className='nav-item nav-link' to='/login'>
+          <span className='nav-item nav-link text-info'>Carlos</span>
+          <button className='nav-item nav-link btn' onClick={handleLogout}>
             Logout
-          </NavLink>
+          </button>
         </ul>
       </div>
     </nav>
