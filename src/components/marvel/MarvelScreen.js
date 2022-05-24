4037fc0462b53fd163a528c344d2aa33e5cde12c
Carlos Londono <c.londono.m@gmail.com>
2022-05-24 17:56:50 -0500

    build Search Screen

--- src/components/marvel/MarvelScreen.js
+++ src/components/marvel/MarvelScreen.js
@@ -3,7 +3,7 @@ import { HeroList } from '../hero/HeroList';
 export const MarverlScreen = () => {
   return (
     <div>
-      <h1>MarverlScreen</h1>
+      <h1>Marverl Screen</h1>
       <hr />
       <HeroList publisher='Marvel Comics' />
     </div>
--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,7 +1,39 @@
+import { useForm } from '../../hooks/useForm';
+
 export const SearchScreen = () => {
+  const [formValues, handleInputChange] = useForm({
+    searchText: '',
+  });
+
+  const { searchText } = formValues;
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    console.log(searchText);
+  };
+
   return (
-    <div>
-      <h1>SearchScreen</h1>
-    </div>
+    <>
+      <h1>Search</h1>
+      <hr />
+      <div className='row'>
+        <div className='col-5'>
+          <form onSubmit={handleSearch}>
+            <input
+              type='text'
+              placeholder='Search hero'
+              className='form-control'
+              name='searchText'
+              autoComplete='off'
+              value={searchText}
+              onChange={handleInputChange}
+            />
+            <button className='btn btn-primary mt-2' type='submit'>
+              Search...
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
