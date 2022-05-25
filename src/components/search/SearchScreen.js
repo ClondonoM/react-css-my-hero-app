a2e6ed2d99163d592743ce0744af208ac648b245
Carlos Londono <c.londono.m@gmail.com>
2022-05-24 19:12:13 -0500

    query string implemented

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,15 +1,27 @@
+import { useNavigate, useLocation } from 'react-router-dom';
+import queryString from 'query-string';
 import { useForm } from '../../hooks/useForm';
+import { getHeroesByName } from '../../selectors/getHeroesByName';
+import { HeroCard } from '../hero/HeroCard';
 
 export const SearchScreen = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { q = '' } = queryString.parse(location.search);
+
   const [formValues, handleInputChange] = useForm({
-    searchText: '',
+    searchText: q,
   });
 
   const { searchText } = formValues;
 
+  const heroesFiltered = getHeroesByName(q);
+
   const handleSearch = (e) => {
     e.preventDefault();
-    console.log(searchText);
+
+    navigate(`?q=${searchText}`);
   };
 
   return (
@@ -32,6 +44,14 @@ export const SearchScreen = () => {
               Search...
             </button>
           </form>
+        </div>
+
+        <div className='col-7'>
+          <h4>Results</h4>
+          <hr />
+          {heroesFiltered.map((hero) => (
+            <HeroCard key={hero.id} {...hero} />
+          ))}
         </div>
       </div>
     </>
