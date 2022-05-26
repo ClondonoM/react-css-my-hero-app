c37470a749075efece6dc4a4d8177f647d9bd37b
Carlos Londono <c.londono.m@gmail.com>
2022-05-25 21:04:20 -0500

    login screen configurated

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,13 +1,23 @@
+import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/authContext';
+import { types } from '../../types/types';
 
 export const LoginScreen = () => {
   const navigate = useNavigate();
+  const { dispatch } = useContext(AuthContext);
   const handleLogin = () => {
+    const action = {
+      type: types.login,
+      payload: { name: 'Carlos' },
+    };
+    dispatch(action);
     navigate('/marvel', { replace: true });
   };
+
   return (
     <div className='container mt-5'>
-      <h1>LoginScreen</h1>
+      <h1>Login</h1>
       <hr />
       <button className='btn btn-primary' onClick={handleLogin}>
         Login
--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,7 +1,11 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/authContext';
 
 export const Navbar = () => {
+  const { user } = useContext(AuthContext);
   const navigate = useNavigate();
+
   const handleLogout = () => {
     navigate('/login', { replace: true });
   };
@@ -30,7 +34,7 @@ export const Navbar = () => {
 
       <div className='navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end'>
         <ul className='navbar-nav ml-auto'>
-          <span className='nav-item nav-link text-info'>Carlos</span>
+          <span className='nav-item nav-link text-info'>{user.name}</span>
           <button className='nav-item nav-link btn me-3' onClick={handleLogout}>
             Logout
           </button>
